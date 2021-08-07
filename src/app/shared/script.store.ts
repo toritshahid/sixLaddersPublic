192fe6f9a8d8dde2384d737d43cacaafa598169c
toritshahid <torit.shahid@gmail.com>
2021-08-07 09:52:32 +0530

    lazy loading implemented

---
+++ src/app/shared/script.store.ts
@@ -0,0 +1,10 @@
+interface Scripts {
+  name: string;
+  src: string;
+}
+export const ScriptStore: Scripts[] = [
+  {name: 'main', src: 'assets/js/main.js'},
+  {name: 'carousel', src: 'assets/js/carousel.js'},
+  {name: 'carouselSlick', src: 'assets/carousel/slick-1.8.1/slick/slick.js'},
+  {name: 'JQuery', src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'}
+];
